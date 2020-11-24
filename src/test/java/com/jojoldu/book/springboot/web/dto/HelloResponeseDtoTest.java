69906a2729ae1c4bdf74e40fe84be9d72e554875
8489545 <rebob9545@gmail.com>
2020-11-24 19:50:55 +0900

    TDD2단계

---
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponeseDtoTest.java
@@ -0,0 +1,27 @@
+package com.jojoldu.book.springboot.web.dto;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponeseDtoTest {
+    @Test
+    public void lokboktest(){
+
+        String name="wow";
+        int amount=1000;
+
+        //값을 전달
+        HelloResponseDto dto = new HelloResponseDto(name,amount);
+
+        //비교
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+        //assertThat
+        //assertj라는 테스트 검증 라이브러리의 검증메소드
+        //검증하고 싶은 대상을 메소드 인자로 받는다.
+        //메소드 체이닝이 지원되어 isEqualTo와 같이 메소드를 이어서 사용할 수 있다.
+
+        //isEqualTo
+        //assertj의 동등 비교 메소드이다.
+        //assertThat에 있는 값을 비교해서 같을 때만 성공.
+    }
+}
